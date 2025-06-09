79a91f4f81d185cf46d42c4f1c0889dd5ef6cc96
Kim Adams <kim.adams@snowflake.com>
2025-06-09 11:17:21 -0400

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,22 @@
-select current_region();
+DECLARE
+    RES RESULTSET ;
+BEGIN
+    show procedures in database ADMIN_UTILITIES;
+    RES :=
+        (select "catalog_name", "schema_name", "name","description"
+        from table(result_scan(last_query_id()))
+        where "schema_name" = 'LOCAL_ADMIN_UTILITES');
+    RETURN TABLE(RES);
+END;
+
+
+DECLARE
+    RES RESULTSET ;
+BEGIN
+    show tasks in database ADMIN_UTILITIES;
+    RES :=
+        (select "database_name", "schema_name", "name","definition"
+        from table(result_scan(last_query_id()))
+        where "schema_name" = 'LOCAL_ADMIN_UTILITES');
+    RETURN TABLE(RES);
+END;
