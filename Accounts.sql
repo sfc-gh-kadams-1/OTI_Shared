0e54c135b9d9dfcbd93be96d5bd25c3be18eef8d
kim.adams@snowflake.com <kim.adams@snowflake.com>
2025-06-30 18:18:37 +0000

    adds

--- Accounts.sql
+++ Accounts.sql
@@ -1,27 +1,27 @@
 /*
-DANY	 - 	usgovvirginia.DANY.azure.snowflakecomputing.com
-DEP	 - 	east-us-2.DEP.azure.snowflakecomputing.com
-DOT	 - 	east-us-2.DOT.azure.snowflakecomputing.com
-FDNY_ANALYTICS	 - 	east-us-2.FDNY_ANALYTICS.azure.snowflakecomputing.com
-MOCJ_GOV	 - 	usgovvirginia.MOCJ_GOV.azure.snowflakecomputing.com
-MOCJ_GOV_PRD	 - 	usgovvirginia.MOCJ_GOV_PRD.azure.snowflakecomputing.com
-NYCPS	 - 	usgovvirginia.NYCPS.azure.snowflakecomputing.com
-ODA_GOV_NONPRD	 - 	usgovvirginia.ODA_GOV_NONPRD.azure.snowflakecomputing.com
-OTI_DEV	 - 	usgovvirginia.OTI_DEV.azure.snowflakecomputing.com
-OTI_GOV_DE_DEV	 - 	usgovvirginia.OTI_GOV_DE_DEV.azure.snowflakecomputing.com
-OTI_GOV_DE_PRD	 - 	usgovvirginia.OTI_GOV_DE_PRD.azure.snowflakecomputing.com
-OTI_GOV_DE_STG	 - 	usgovvirginia.OTI_GOV_DE_STG.azure.snowflakecomputing.com
-OTI_GOV_DE_TST	 - 	usgovvirginia.OTI_GOV_DE_TST.azure.snowflakecomputing.com
-OTI_GOV_DEV	 - 	usgovvirginia.OTI_GOV_DEV.azure.snowflakecomputing.com
-OTI_GOV_PRD	 - 	usgovvirginia.OTI_GOV_PRD.azure.snowflakecomputing.com
-OTI_GOV_STG	 - 	usgovvirginia.OTI_GOV_STG.azure.snowflakecomputing.com
-OTI_GOV_TST	 - 	usgovvirginia.OTI_GOV_TST.azure.snowflakecomputing.com
-OTI_ODA_DEV	 - 	
-OTI_ODA_PRD	 - 	
-OTI_PRD	 - 	east-us-2.OTI_PRD.azure.snowflakecomputing.com
-OTI_STG	 - 	east-us-2.OTI_STG.azure.snowflakecomputing.com
-OTI_TST	 - 	east-us-2.OTI_TST.azure.snowflakecomputing.com
-QDA	 - 	usgovvirginia.QDA.azure.snowflakecomputing.com
-QDA_NONPRD	 - 	usgovvirginia.QDA_NONPRD.azure.snowflakecomputing.com
+DANY	 - 	usgovvirginia.AK84850.azure.snowflakecomputing.com
+DEP	 - 	east-us-2.AUA26560.azure.snowflakecomputing.com
+DOT	 - 	east-us-2.BOA08692.azure.snowflakecomputing.com
+FDNY_ANALYTICS	 - 	east-us-2.LZA23633.azure.snowflakecomputing.com
+MOCJ_GOV	 - 	usgovvirginia.IW03620.azure.snowflakecomputing.com
+MOCJ_GOV_PRD	 - 	usgovvirginia.MW55991.azure.snowflakecomputing.com
+NYCPS	 - 	usgovvirginia.HU63684.azure.snowflakecomputing.com
+ODA_GOV_NONPRD	 - 	usgovvirginia.BX44575.azure.snowflakecomputing.com
+OTI_DEV	 - 	usgovvirginia.HS78970.azure.snowflakecomputing.com
+OTI_GOV_DE_DEV	 - 	usgovvirginia.CG20270.azure.snowflakecomputing.com
+OTI_GOV_DE_PRD	 - 	usgovvirginia.ED35342.azure.snowflakecomputing.com
+OTI_GOV_DE_STG	 - 	usgovvirginia.VC94894.azure.snowflakecomputing.com
+OTI_GOV_DE_TST	 - 	usgovvirginia.YI73495.azure.snowflakecomputing.com
+OTI_GOV_DEV	 - 	usgovvirginia.OB60766.azure.snowflakecomputing.com
+OTI_GOV_PRD	 - 	usgovvirginia.HW08838.azure.snowflakecomputing.com
+OTI_GOV_STG	 - 	usgovvirginia.QF97186.azure.snowflakecomputing.com
+OTI_GOV_TST	 - 	usgovvirginia.ZA52489.azure.snowflakecomputing.com
+OTI_ODA_DEV	 - 	.ZSB19053.azure.snowflakecomputing.com
+OTI_ODA_PRD	 - 	.THB36563.azure.snowflakecomputing.com
+OTI_PRD	 - 	east-us-2.OF23789.azure.snowflakecomputing.com
+OTI_STG	 - 	east-us-2.CV47234.azure.snowflakecomputing.com
+OTI_TST	 - 	east-us-2.XI62875.azure.snowflakecomputing.com
+QDA	 - 	usgovvirginia.QW85485.azure.snowflakecomputing.com
+QDA_NONPRD	 - 	usgovvirginia.KA07005.azure.snowflakecomputing.com
 YR44338	 - 	usgovvirginia.YR44338.azure.snowflakecomputing.com
 */
